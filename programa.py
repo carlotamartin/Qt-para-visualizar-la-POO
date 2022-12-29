8922c1cacd0aa9dacd855bcde556b8b894647486
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2022-12-29 11:10:39 +0100

    primera prueba

--- programa.py
+++ programa.py
@@ -1,10 +1,18 @@
-from PySide6.QtWidgets import QApplication, QWidget, QPushButton
+from PySide6.QtWidgets import QMainWindow, QApplication, QWidget, QPushButton
 import sys
 # Creamos una aplicación para gestionar la interfaz
 app = QApplication(sys.argv)
-# Creamos un widget para generar la ventana
-window = QPushButton("Hola mundo")
+# Ahora la ventana la gestiona el widget de ventana principal
+window = QMainWindow()
+# Damos un título a la ventana principal
+window.setWindowTitle("Hola mundo")
+
+
+# Guardamos el botón en una variable
+button = QPushButton("Soy un botón")
+# Establecemos el botón como widget central de la ventana principal
+window.setCentralWidget(button)
 # Mostramos la ventana, se encuentra oculta por defecto
 window.show()
 # Iniciamos el bucle del programa
-sys.exit(app.exec_())
+sys.exit(app.exec())
